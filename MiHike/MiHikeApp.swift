84aefa43442dffc0bea38ed38b2bc56f37a06886
hoover13 <hoovermtk25@gmail.com>
2024-02-02 14:43:15 -0500

    Initial Commit

---
+++ MiHike/MiHikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiHikeApp.swift
+//  MiHike
+//
+//  Created by Min Thu Khine on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct MiHikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SplashScreenView()
+        }
+    }
+}
---
+++ MiHike/SplashScreenView.swift
@@ -0,0 +1,38 @@
+//
+//  SplashScreenView.swift
+//  MiHike
+//
+//  Created by Min Thu Khine on 2/2/24.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    @State private var isActive = false
+    
+    var body: some View {
+        
+        if isActive {
+            ContentView()
+        } else {
+            ZStack {
+                Image("bg")
+                    .resizable()
+                    .ignoresSafeArea()
+                
+                Image("logo object")
+                    .resizable()
+                    .frame(width: 200, height: 200)
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                    self.isActive = true
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
